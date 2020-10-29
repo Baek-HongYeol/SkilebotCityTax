ab7545b6f57ff1ef02bd68e05c2513297694555f
Baek-HongYeol <bac25450@gmail.com>
2020-10-30 00:44:36 +0900

    abstract function parsing effect and applying to multiples

--- SkilebotCityTaxCalc.c
+++ SkilebotCityTaxCalc.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int alphatoint(char a);
+double alphatofloat(const char* cast_str);
+
 int price_input(char* str) {
     int price = 0;
     int errnum = 0;
@@ -75,12 +78,163 @@ int buildingInput(char* str, int size) {
         i++;
     }
     if (i < size - 1) return 0;
-    while ((ch = getchar()) != '\n') {  // If i==7, then getchar() == \n
+    while ((ch = getchar()) != '\n') {  // If i==size-1, then getchar() == \n. in condition of short circuit evaluation
         overcount++;        // 7자리를 넘어서 입력한 경우
     }
     if (overcount > 0) return 0;
     str[i] = '\0';
     return check_buildings(str, size);
+}
+
+
+void applyEffect(float* multiples, char* buildings) {
+    char b_arr[15] = { 'x', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', NULL};
+    char gontor[] = "1.10#0.5";                     //"1.10#0.5\0"; 
+    char house[] = "0";
+    char convenience[] = "2.221_3_#4\0.312_#0.7";   // num_ef. ('.',  place, num_build, build1_, build2_, ... , #coefficient'\0')
+    char school[] = "2.411_#3\0.218_#0.5";
+    char* effects[] = { gontor, house, convenience, school };
+
+    for (int address = 0; address < strlen(buildings); address++) {
+        char building = buildings[address];
+        char* b_idx = strchr(b_arr, building);   // b의 주소를 반환, 없으면 0.
+        int type_num = b_idx - b_arr;
+        char* effect = effects[type_num];
+
+        if (!b_idx) {   // idx
+            printf("Error not permitted character in applyEffect()\n");
+            return;
+        }
+        {
+            int num_ef = alphatoint(effect[0]);
+            int k, i = 2;
+            for (int j = 0; j < num_ef; j++) {      // 효과 개수만큼 반복
+                unsigned char first_efbuild = 0;        // b0 0 0 0  0 0 0 0 
+                unsigned char second_efbuild = 0;
+                int place = alphatoint(effect[i]);  // i==1
+                i++;
+                int num_build = alphatoint(effect[i]);  // i==2
+                if (num_build == 0) {
+                    first_efbuild = 255;
+                    second_efbuild = 63;
+                    k = i + 1;              // move k to index of '#'. effect = ".10#0.7"
+                }
+                else {
+                    int n = 0;
+                    for (k = i + 1; effect[k] != '#'; k++) {
+                        if (effect[k] == '_') {
+                            if (n != 0) {
+                                if (n < 8) {
+                                    first_efbuild += 1 << n;
+                                }
+                                else {
+                                    second_efbuild += 1 << (n - 8);
+                                }
+                            }
+                            n = 0;
+                            continue;
+                        }
+                        else if (n == 0)
+                            n = alphatoint(effect[k]);
+                        else {
+                            n *= 10;
+                            n += alphatoint(effect[k]);
+                        }
+                    }
+                }
+                k++;
+                double coefficient = atof(&effect[k]);  // # 이후 '\0'까지 실수로 변환
+                i = k + strlen(&effect[k]) + 2;       // # 이후 다음 effect로 이동
+
+                if (first_efbuild < 1 && second_efbuild < 1) {
+                    continue;
+                }
+                for (int address1 = 0; address1 < strlen(buildings); address1++) {  // 각 효과를 buildings 문자열을 탐색하여 multiples에 적용
+                    if (address1 == address) continue;
+
+                    if (place == 0 || abs(address1 - address) <= place) { 
+                        char* neighbor = strchr(b_arr, buildings[address1]);   // b의 주소를 반환, 없으면 0.
+                        int neighbor_type = neighbor - b_arr;
+                        if (neighbor_type < 8 && (first_efbuild & 1 << neighbor_type)) {
+                            multiples[address1] *= coefficient;
+                        }
+                        else if (second_efbuild & (1 << (neighbor_type - 8))) {
+                            multiples[address1] *= coefficient;
+                        }
+                    }
+                }
+            }
+
+        }
+        printf("multiples: ");
+        for (int i = 0; i < 7; i++) {
+            printf("%f, ", multiples[i]);
+        }
+        printf("\n");
+    }
+    /*{('공터', 1, (1, 0, 0.5)),
+        ('주택', 0), ('편의점', 2, (2, 2, 1, 3, 4), (3, 1, 2, 0.7)), ('학교', 2, (4, 1, 1, 3), (2, 1, 8, 0.5)),
+        ('회사', 1, (3, 10, 1, 2, 5, 6, 7, 8, 10, 11, 12, 13, 2)), ('병원', 1, (0, 0, 1.5)),
+        ('은행', 1, (1, 1, 5, 5)),
+        ('백화점', 2, (1, 0, 3, 5), (0, 1, 2, 0)),
+        ('호텔', 2, (0, 9, 2, 4, 5, 6, 7, 10, 11, 12, 13, 2), (0, 1, 1, 0.5)),
+        ('카지노', 3, (2, 1, 1, 0.5), (0, 1, 9, 0.5), (1, 1, 8, 2)), ('항구', 1, (0, 3, 8, 9, 10, 3)),
+        ('경기장', 1, (2, 0, 0.2)), ('교회', 1, [2, 2, 1, 4, 1.5]), ('공장', 1, (3, 0, 0)))*/
+        // ('건물이름', 효과 개수, (place, 건물종류개수, 종류,.. , 효과)..)
+        // 효과 개수 == 0, nothing
+        // place == 0, 전범위,
+        // 건물종류개수 == 0, 모든 건물, 종류X
+}
+
+void loop() {
+    int green = 0, lus = 0;
+    int weekend = 0;    //기본값은 평일, 1-토요일, 2-일요일
+    char ch;
+    green = price_input("그린건설");
+    lus = price_input("러스관광");
+
+    printf("산출일이 주말인지 입력하세요.(y/N) ");
+    ch = commandInput();
+    while (ch != 'y' && ch != 'n' && ch != '\n') {
+        ch = commandInput();
+    }
+    if (ch == 'y') {
+        printf("일요일이면 y, 토요일이면 n을 입력하세요.(y/N) ");
+        ch = commandInput();
+        while (ch != 'y' && ch != 'n' && ch != '\n') {
+            ch = commandInput();
+        }
+        if (ch == 'y') weekend = 2;
+        else weekend = 1;
+    }
+    printf("공터=x, 주택=a, 편의점=b, 학교=c, 회사=d\n");
+    printf("병원=e, 은행=f, 백화점-g, 호텔=h, 카지노=i\n");
+    printf("항구=j, 경기장=k, 교회=l, 공장=m\n");
+    printf("배열할 건물을 띄어쓰기 없이 7자리 적으세요: ");
+
+    char buildings[8];
+    ch = buildingInput(buildings, sizeof(buildings)/sizeof(char));
+    while (!ch) {
+        printf("잘못 입력하셨습니다.\n");
+        printf("배열할 건물을 띄어쓰기 없이 7자리 적으세요: ");
+        ch = buildingInput(buildings, sizeof(buildings) / sizeof(char));
+    }
+
+    float multiples[7] = { 1, 1, 1, 1, 1, 1, 1 };
+    
+    applyEffect(multiples, buildings);
+
+    printf("그린: %d, 러스: %d\n", green, lus);
+    printf("weekend: %d\n", weekend);
+    printf("buildings: %s\n", buildings);
+    printf("buildings log: %d\n", ch);
+
+}
+int main() {
+    printf("슷칼봇 도시의 집세 계산 프로그램입니다.\n");
+    //printf("프로그램을 종료하시려면 q를 리셋하려면 r을 입력하세요.\n");
+    loop();
+    return 0;
 }
 
 int alphatoint(char a) {
@@ -123,116 +277,4 @@ double alphatofloat(const char* cast_str) {
         }
     }
     return sign * (Dec_Val + Dou_val);
-}
-
-void loop() {
-    int green = 0, lus = 0;
-    int weekend = 0;    //기본값은 평일, 1-토요일, 2-일요일
-    char ch;
-    green = price_input("그린건설");
-    lus = price_input("러스관광");
-
-    printf("산출일이 주말인지 입력하세요.(y/N) ");
-    ch = commandInput();
-    while (ch != 'y' && ch != 'n' && ch != '\n') {
-        ch = commandInput();
-    }
-    if (ch == 'y') {
-        printf("일요일이면 y, 토요일이면 n을 입력하세요.(y/N) ");
-        ch = commandInput();
-        while (ch != 'y' && ch != 'n' && ch != '\n') {
-            ch = commandInput();
-        }
-        if (ch == 'y') weekend = 2;
-        else weekend = 1;
-    }
-    printf("공터=x, 주택=a, 편의점=b, 학교=c, 회사=d\n");
-    printf("병원=e, 은행=f, 백화점-g, 호텔=h, 카지노=i\n");
-    printf("항구=j, 경기장=k, 교회=l, 공장=m\n");
-    printf("배열할 건물을 띄어쓰기 없이 7자리 적으세요: ");
-
-    char buildings[8];
-    ch = buildingInput(buildings, sizeof(buildings)/sizeof(char));
-    while (!ch) {
-        printf("잘못 입력하셨습니다.\n");
-        printf("배열할 건물을 띄어쓰기 없이 7자리 적으세요: ");
-        ch = buildingInput(buildings, sizeof(buildings) / sizeof(char));
-    }
-
-    char b_key[14] = { 'x', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' };
-    int multiples[7] = { 1, 1, 1, 1, 1, 1, 1 };
-
-    char gontor[] = "1.10#0.5";                     //"1.10#0.5\0";      
-    char convenience[] = "2.221_3_#4\0.312_#0.7";   // num_ef. ('.',  place, num_build, build1_, build2_, ... , #efficient'\0')
-    {
-        unsigned char first_efbuild = 0;        // b0 0 0 0  0 0 0 0 
-        unsigned char second_efbuild = 0;
-        int num_ef = alphatoint(gontor[0]);
-        int k, i = 2;
-        for (int j = 0; j < num_ef; j++) {
-            int place = alphatoint(gontor[i]);  // i==1
-            i++;
-            int num_build = alphatoint(gontor[i]);  // i==2
-            if (num_build == 0) {       
-                first_efbuild = 255;
-                second_efbuild = 63;
-                k = i + 1;              // move k to index of '#'. effect = ".10#0.7"
-            }
-            else {
-                int n = 0;
-                for (k = i + 1; gontor[k] != '#'; k++) {
-                    if (gontor[k] == '_') {
-                        if (n != 0) {
-                            if (n < 8) {
-                                first_efbuild += 1 << n;
-                            }
-                            else {
-                                second_efbuild += 1 << (n - 8);
-                            }
-                        }
-                        n = 0;
-                        continue;
-                    }
-                    else if (n == 0)
-                        n = alphatoint(gontor[k]);
-                    else {
-                        n *= 10;
-                        n += alphatoint(gontor[k]);
-                    }
-                }
-            }
-            
-            k++;
-            double efficient = atof(&gontor[k]);  // # 이후 '\0'까지 실수로 변환
-            printf("place: %d\n", place);
-            printf("num_build = %d\n", num_build);
-            printf("efficient = %lf\n", efficient);
-            i = k + strlen(&gontor[k]) + 2;
-        }
-        
-    }
-    /*{('공터', 1, (1, 0, 0.5)),
-        ('주택', 0), ('편의점', 2, (2, 2, 1, 3, 4), (3, 1, 2, 0.7)), ('학교', 2, (4, 1, 1, 3), (2, 1, 8, 0.5)),
-        ('회사', 1, (3, 10, 1, 2, 5, 6, 7, 8, 10, 11, 12, 13, 2)), ('병원', 1, (0, 0, 1.5)),
-        ('은행', 1, (1, 1, 5, 5)),
-        ('백화점', 2, (1, 0, 3, 5), (0, 1, 2, 0)),
-        ('호텔', 2, (0, 9, 2, 4, 5, 6, 7, 10, 11, 12, 13, 2), (0, 1, 1, 0.5)),
-        ('카지노', 3, (2, 1, 1, 0.5), (0, 1, 9, 0.5), (1, 1, 8, 2)), ('항구', 1, (0, 3, 8, 9, 10, 3)),
-        ('경기장', 1, (2, 0, 0.2)), ('교회', 1, [2, 2, 1, 4, 1.5]), ('공장', 1, (3, 0, 0)))*/
-    // ('건물이름', 효과 개수, (place, 건물종류개수, 종류,.. , 효과)..)
-    // 효과 개수 == 0, nothing
-    // place == 0, 전범위,
-    // 건물종류개수 == 0, 모든 건물, 종류X
-
-    printf("그린: %d, 러스: %d\n", green, lus);
-    printf("weekend: %d\n", weekend);
-    printf("buildings: %s\n", buildings);
-    printf("buildings log: %d\n", ch);
-
-}
-int main() {
-    printf("슷칼봇 도시의 집세 계산 프로그램입니다.\n");
-    //printf("프로그램을 종료하시려면 q를 리셋하려면 r을 입력하세요.\n");
-    loop();
-    return 0;
 }
